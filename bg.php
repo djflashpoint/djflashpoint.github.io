07bc9549775c769339671b51568773bcb1d297fa
djflashpoint <djflashpoint@users.noreply.github.com>
2017-12-29 10:38:57 -0800

    Update bg.php

--- bg.php
+++ bg.php
@@ -6,7 +6,7 @@ Latest version always at:
 http://photomatt.net/scripts/randomimage
 */// Make this the relative path to the images, like "../img" or "random/images/".
 // If the images are in the same directory, leave it blank.
-$folder = '../img/';
+$folder = '';
 
 // Space seperated list of extensions, you probably won't have to change this.
 $exts = 'jpg jpeg png gif';
