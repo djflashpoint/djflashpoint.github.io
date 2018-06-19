9e7d89254e453e69276443255583fa54da3ffed2
djflashpoint <djflashpoint@users.noreply.github.com>
2018-06-18 22:00:32 -0700

    Ain't nobody dope as me I'm dressed so fresh so clean

--- css/old/old/bg.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $bg = array('grid.gif', 'floppy.gif' ); // array of filenames
-
-  $i = rand(0, count($bg)-1); // generate random number size of the array
-  $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
-?>
