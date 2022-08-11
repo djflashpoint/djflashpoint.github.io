2b6351a7c518f499ad926c16d93b42c9825e8cf2
Gabe <germ777poc@gmail.com>
2022-08-11 15:38:45 -0700

    timecpp

---
+++ time.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+using namespace std;
+int main()
+{
+    if (true)
+    {
+        time_t now = time(NULL);
+        std::cout << (ctime(&now)) << std::endl;
+        Sleep(1000);
+        system("cls");
+    }
+}
