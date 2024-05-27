215f4a6b3c8ed98125f628b0a82ef29e1bea8574
djflashpoint <germ777poc@gmail.com>
2024-05-27 14:38:37 -0700

    stuff

---
+++ sg/index.js
@@ -0,0 +1,17 @@
+
+        await term.echo("List of Commands:", { typing: true, delay: 50 });
+        await term.echo("start", { typing: true, delay: 50 });
+        await term.echo("about", { typing: true, delay: 50 });
+        await term.echo("rabbit", { typing: true, delay: 50 });
+        await term.echo("figlet", { typing: true, delay: 50 });
+        await term.echo("time", { typing: true, delay: 50 });
+        await term.echo("micro", { typing: true, delay: 50 });
+        await term.echo("microext", { typing: true, delay: 50 });
+        await term.echo("stream", { typing: true, delay: 50 });
+        await term.echo("streamext", { typing: true, delay: 50 });
+        await term.echo("neo", { typing: true, delay: 50 });
+        await term.echo("foo", { typing: true, delay: 50 });
+        await term.echo("ls", { typing: true, delay: 50 });
+        await term.echo("clock", { typing: true, delay: 50 });
+        await term.echo("dir", { typing: true, delay: 50 });
+        await term.echo("exit", { typing: true, delay: 50 });
