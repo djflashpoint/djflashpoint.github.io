f26081e109fc7371a3993afd6472adfe27021fe9
djflashpoint <germ777poc@gmail.com>
2024-06-02 14:42:02 -0700

    iv

---
+++ invayne/ticker.js
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>404 - 找不到文件或目录。</title>
+<style type="text/css">
+
+</style>
+</head>
+<body bgcolor="d0d0d0" >
+<div id="header"><h1>服务器错误</h1></div>
+<div id="content">
+ <div class="content-container"><fieldset>
+  <h3>亚美官网电话_am8网上注册</h2>
+  <h3>您要查找的资源可能已被删除，已更改名称或者暂时不可用。</h3>
+ </fieldset></div>
+</div>
+
+</body>
+</html>
+<!--
+     FILE ARCHIVED ON 16:32:13 Oct 16, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 20:48:19 Jun 02, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
+<!--
+playback timings (ms):
+  captures_list: 0.897
+  exclusion.robots: 0.109
+  exclusion.robots.policy: 0.096
+  esindex: 0.013
+  cdx.remote: 16.495
+  LoadShardBlock: 99.496 (3)
+  PetaboxLoader3.datanode: 51.634 (4)
+  PetaboxLoader3.resolve: 119.893 (3)
+  load_resource: 177.919
+-->
